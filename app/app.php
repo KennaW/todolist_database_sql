25d0fbe675042d88a84626aaaf8bf043a270a377
Jonathan Lin & Kenna Warsinske <student@epicodus.com>
2015-03-16 14:36:41 -0700

    set up first test

---
+++ app/app.php
@@ -0,0 +1,29 @@
+<?php
+    require_once __DIR__."../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+     $app = new Silex\Application();
+
+     $DB = new PDO('pgsql:host=localhost;dbname=to_do');
+
+     $app->register(new Silex\Provider\TwigServicePRovider(), array(
+         'twig.path' => __DIR__.'/../views'
+     ));
+
+     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.twig')
+     });
+
+     $app->post("/tasks", function() use ($app) {
+         $tasks = new Task($_POST['description']);
+         $task->save();
+         return $app['twig']->render('create_task.twig', array('newtask' => $task))
+     });
+
+     $app->post("/delete_tasks", function() use ($app) {
+         Task::deleteAll();
+         return $app['twig']->render('delete_tasks.twig');
+     });
+
+     return $app;
+?>
