76acdcf1dce89e8ad68d6f6cdc8e965bd5a88ba8
Jonathan Lin & Kenna Warsinske <student@epicodus.com>
2015-03-16 16:30:00 -0700

    add find method. all tests pass

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -49,55 +49,74 @@
              $this->assertEquals([$test_Task, $test_Task2], $result);
 
          }
-         //
-        //  function test_deleteAll()
-        //  {
-        //      //Arrange
-        //      $description = "Wash the dog";
-        //      $description2 = "Water the lawn";
-        //      $id = null;
-        //      $test_Task = new Task($description, $id);
-        //      $test_Task->save();
-        //      $test_Task2 = new Task($description2, $id);
-        //      $test_Task2->save();
-         //
-        //      //Act
-        //      Task::deleteAll();
-         //
-        //      //Assert
-        //      $result = Task::getAll();
-        //      $this->assertEquals([], $result);
-         //
-        //  }
-         //
-        //  function test_getId()
-        //  {
-        //      //Arrange
-        //      $description = "Wash the dog";
-        //      $id = 1;
-        //      $test_Task = new Task($description, $id);
-         //
-        //      //Act
-        //      $result = $test_Task->getId();
-         //
-        //      //Assert
-        //      $this->assertEquals(1, $result);
-        //  }
-         //
-        //  function test_setId()
-        //  {
-        //      //Arrange
-        //      $description = "Wash the dog";
-        //      $id = null;
-        //      $test_Task = new Task($description, $id);
-         //
-        //      //Act
-        //      $test_Task->setId(2);
-         //
-        //      //Assert
-        //      $result = $test_Task->getId();
-        //      $this->assertEquals(2, $result);
-        //  }
+
+         function test_deleteAll()
+         {
+             //Arrange
+             $description = "Wash the dog";
+             $description2 = "Water the lawn";
+             $id = null;
+             $test_Task = new Task($description, $id);
+             $test_Task->save();
+             $test_Task2 = new Task($description2, $id);
+             $test_Task2->save();
+
+             //Act
+             Task::deleteAll();
+
+             //Assert
+             $result = Task::getAll();
+             $this->assertEquals([], $result);
+
+         }
+
+         function test_getId()
+         {
+             //Arrange
+             $description = "Wash the dog";
+             $id = 1;
+             $test_Task = new Task($description, $id);
+
+             //Act
+             $result = $test_Task->getId();
+
+             //Assert
+             $this->assertEquals(1, $result);
+         }
+
+         function test_setId()
+         {
+             //Arrange
+             $description = "Wash the dog";
+             $id = null;
+             $test_Task = new Task($description, $id);
+
+             //Act
+             $test_Task->setId(2);
+
+             //Assert
+             $result = $test_Task->getId();
+             $this->assertEquals(2, $result);
+         }
+
+
+            function test_find()
+            {
+                //Arrange
+                $description = "Wash the dog";
+                $id = null;
+                $description2 = "Water the lawn";
+                $test_Task = new Task($description, $id);
+                $test_Task->save();
+                $test_Task2 = new Task($description2, $id);
+                $test_Task2->save();
+
+                //Act
+                $result = Task::find($test_Task->getId());
+
+                //Assert
+                $this->assertEquals($test_Task, $result);
+            }
 
     }
 ?>
