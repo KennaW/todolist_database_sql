84f16b4ef9bde7efa7e8f0e852b4cfe46a1b5048
Jonathan Lin & Kenna Warsinske <student@epicodus.com>
2015-03-17 11:02:08 -0700

    Add delete categories in app and views

--- app/app.php
+++ app/app.php
@@ -1,29 +1,48 @@
 <?php
     require_once __DIR__."../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
      $app = new Silex\Application();
 
      $DB = new PDO('pgsql:host=localhost;dbname=to_do');
 
-     $app->register(new Silex\Provider\TwigServicePRovider(), array(
+     $app->register(new Silex\Provider\TwigServiceProvider(), array(
          'twig.path' => __DIR__.'/../views'
      ));
 
      $app->get("/", function() use ($app) {
-        return $app['twig']->render('tasks.twig', array('tasks' => Task:: getALl()));
+        return $app['twig']->render('index.twig');
      });
 
      $app->post("/tasks", function() use ($app) {
-         $tasks = new Task($_POST['description']);
+         return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+     });
+
+     $app->get("/categories", function() use ($app) {
+         return $app['twig']->render('catgories.twig', array('categories' => Category::getAll()));
+     });
+
+     $app->post("/tasks", function() use ($app){
+         $task = new Task($_POST['description']);
          $task->save();
-         return $app['twig']->render('create_task.twig', array('newtask' => $task));
+         return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
      });
 
      $app->post("/delete_tasks", function() use ($app) {
          Task::deleteAll();
-         return $app['twig']->render('delete_tasks.twig');
+         return $app['twig']->render('index.twig');
      });
 
+     $app->post("/categories", function() use ($app){
+         $category = new Category($_POST['name']);
+         $category->save();
+         return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+     });
+
+     $app->post("/delete_categories", function() use ($app){
+         Category::deleteAll();
+         return $app['twig']->render('index.twig');
+     });
      return $app;
 ?>
